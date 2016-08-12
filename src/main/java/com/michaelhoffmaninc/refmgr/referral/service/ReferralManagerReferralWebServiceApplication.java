80010224f774bd39346f313bed4348fde8a10b1c
Michael Hoffman <mike@michaelhoffmaninc.com>
2016-08-12 18:37:01 -0500

    Initial project

---
+++ src/main/java/com/michaelhoffmaninc/refmgr/referral/service/ReferralManagerReferralWebServiceApplication.java
@@ -0,0 +1,12 @@
+package com.michaelhoffmaninc.refmgr.referral.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReferralManagerReferralWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReferralManagerReferralWebServiceApplication.class, args);
+	}
+}
